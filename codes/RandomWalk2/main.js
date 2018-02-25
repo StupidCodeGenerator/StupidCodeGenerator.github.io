36e111f09c2c254461a3b0ad69a0f6e46c44ffe3
StupidCodeGenerator <c@thebgame.com>
2018-02-25 11:08:37 +0800

    update

--- codes/RandomWalk2/main.js
+++ codes/RandomWalk2/main.js
@@ -5,6 +5,8 @@ var randomArray;
 
 var randomResultArray;
 
+var X_OFFSET = 50;
+
 function init(){
     randomResultArray = new Array();
     for(var i = -100 ; i < 100; i ++){
@@ -14,9 +16,11 @@ function init(){
 
 function update(){
 
+    var TIMES = canvas.width / 2;
+
     randomArray = new Array();
 
-    for(var i = 0 ; i < 1000; i ++){
+    for(var i = 0 ; i < TIMES; i ++){
         var random = GetRandom11();
         currentValue += random;
         randomArray.push(currentValue);
@@ -36,7 +40,7 @@ function update(){
     context.fillStyle = "black";
 
     for(var i = 0 ; i < randomArray.length && i < canvas.width ; i ++){
-        context.rect(i + 50, canvas.height / 2 - randomArray[i], 1, 1);
+        context.rect(i + X_OFFSET, canvas.height / 2 - randomArray[i], 1, 1);
     }
 
     context.textAlign = "start"
@@ -48,8 +52,8 @@ function update(){
 
     for(var i = -100 ; i < 100 ; i ++){
         if(randomResultArray[i] > 0){
-            context.moveTo(1000, i + canvas.height / 2);
-            context.lineTo(1000 + randomResultArray[i], i + canvas.height / 2);
+            context.moveTo(TIMES + X_OFFSET, i + canvas.height / 2);
+            context.lineTo(TIMES + X_OFFSET - randomResultArray[i], i + canvas.height / 2);
         }
     }
 
