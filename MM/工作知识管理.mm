1f01ff9279dd61b014d790ca4f1f840a577023ca
Chen <c@thebgame.com>
2020-06-01 19:02:49 +0800

    update

---
+++ MM/工作知识管理.mm
@@ -0,0 +1,90 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="工作知识管理" FOLDED="false" ID="ID_266480184" CREATED="1591004714525" MODIFIED="1591004728168" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="工作需求" POSITION="right" ID="ID_1737034388" CREATED="1591005953223" MODIFIED="1591005971317">
+<edge COLOR="#0000ff"/>
+<node TEXT="HTTP逆向？" ID="ID_271147434" CREATED="1591005973084" MODIFIED="1591005979884"/>
+<node TEXT="Android逆向？" ID="ID_174850714" CREATED="1591005982838" MODIFIED="1591005985605"/>
+</node>
+<node TEXT="Android逆向" POSITION="right" ID="ID_20948288" CREATED="1591004729137" MODIFIED="1591004731637">
+<edge COLOR="#ff0000"/>
+<node TEXT="AndroidNDK" ID="ID_913758728" CREATED="1591004748620" MODIFIED="1591004757717"/>
+<node TEXT="JNI" ID="ID_566726702" CREATED="1591004758123" MODIFIED="1591004759373"/>
+<node TEXT="Android 虚拟机" ID="ID_1694904403" CREATED="1591004763639" MODIFIED="1591004767078"/>
+<node TEXT="汇编语言，ARM， Smali， IPC" ID="ID_613399128" CREATED="1591004771159" MODIFIED="1591004784227"/>
+<node TEXT="抓包工具" ID="ID_1050797009" CREATED="1591004786807" MODIFIED="1591004793013">
+<node TEXT="抓包工具是咋写的？" ID="ID_1316913056" CREATED="1591004794686" MODIFIED="1591004799374"/>
+</node>
+<node TEXT="脚本，爬虫" ID="ID_510471084" CREATED="1591004805001" MODIFIED="1591004808018"/>
+<node TEXT="adb, linux" ID="ID_615685764" CREATED="1591004900330" MODIFIED="1591004904616"/>
+<node TEXT="加密与解密" ID="ID_647248020" CREATED="1591004924003" MODIFIED="1591004931585"/>
+<node TEXT="smali 代码调试" ID="ID_1041849339" CREATED="1591005624311" MODIFIED="1591005629563">
+<node TEXT="Dalvik" ID="ID_1205582592" CREATED="1591005832528" MODIFIED="1591005842887"/>
+</node>
+<node TEXT="Android系统原理" ID="ID_1296501611" CREATED="1591005633814" MODIFIED="1591005637893"/>
+</node>
+</node>
+</map>
